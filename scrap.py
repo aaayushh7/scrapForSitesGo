2f12946d1e053b8ec727239c10713e268d60c7ec
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-21 20:29:56 +0530

    All names of left panel scrapped

--- scrap.py
+++ scrap.py
@@ -17,86 +17,102 @@ Allnames =[]
 Allmails=[]
 Links=[]
 #   LEFT PANEL
-panelLeft = main_parsys.find('div', class_ = 'col-sm-4 panel-builder-33-col panel-builder-left')
-image = panelLeft.find('div', class_ = 'image' )
-img_tag = image.find('img')
-img_src = img_tag.get('src','')
-divName = panelLeft.find('div', class_ = 'text col-md-8')
-innerDivName = divName.find('p').find('a')
-name = innerDivName.get_text(strip=True)
-# print(name)
-Allnames.append(name)
-# print(img_src)
-imageURL.append(img_src)
-innerLink = main_parsys.find('a', href=True)
-innerURL = innerLink['href']
-# print(innerURL)
-Links.append(innerURL)
-innerResponse = requests.get(innerURL)
-innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
+panelLeft = main_parsys.findAll('div', class_ = 'col-sm-4 panel-builder-33-col panel-builder-left')
 
-contact = innerSoup.find('div', 'contact-info primary')
-if contact:
+for panel in panelLeft:
+    divName = panel.find('div', class_='text col-md-8')
+    if divName:
+        # Process divName as needed
+        innerDivName = divName.find('p').find('a')
+        if innerDivName:
+            name = innerDivName.get_text(strip=True)
+            Allnames.append(name)
+        else:
+            Allnames.append("Name not found")
+    else:
+        print("No matching div found in this panel.")
 
-    contact2 = contact.find('a')
-    email = contact2.get_text(strip=True)
-    # print(email)
-    Allmails.append(email)
-else:
-    Allmails.append("Not Found")
+print("Scrapped names are: ", Allnames)
+
+# image = panelLeft.find('div', class_ = 'image' )
+# img_tag = image.find('img')
+# img_src = img_tag.get('src','')
+# divName = panelLeft.find('div', class_ = 'text col-md-8')
+# innerDivName = divName.find('p').find('a')
+# name = innerDivName.get_text(strip=True)
+# # print(name)
+# Allnames.append(name)
+# # print(img_src)
+# # imageURL.append(img_src)
+# innerLink = main_parsys.find('a', href=True)
+# innerURL = innerLink['href']
+# # print(innerURL)
+# Links.append(innerURL)
+# innerResponse = requests.get(innerURL)
+# innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
+#
+# contact = innerSoup.find('div', 'contact-info primary')
+# if contact:
+#
+#     contact2 = contact.find('a')
+#     email = contact2.get_text(strip=True)
+#     # print(email)
+#     Allmails.append(email)
+# else:
+#     Allmails.append("Not Found")
 # innerResponse = requests.get(innerURL)
 # innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
 # print(innerSoup)
 
 #MIDPANEL
-panelMid = main_parsys.find('div', class_='col-sm-4 panel-builder-33-col panel-builder-mid')
-image = panelMid.find('div', class_ = 'image' )
-img_tag = image.find('img')
-img_src = img_tag.get('src','')
-divName = panelMid.find('div', class_ = 'text col-md-8')
-innerDivName = divName.find('p').find('a')
-name = innerDivName.get_text(strip=True)
-# print(name)
-Allnames.append(name)
-# print(img_src)
-imageURL.append(img_src)
-innerLink = panelMid.find('a', href=True)
-innerURL = innerLink['href']
-# print(innerURL)
-Links.append(innerURL)
-innerResponse = requests.get(innerURL)
-innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
-contact = innerSoup.find('div', 'contact-info primary')
-contact2 = contact.find('a')
-email = contact2.get_text(strip=True)
-# print(email)
-Allmails.append(email)
+# panelMid = main_parsys.find('div', class_='col-sm-4 panel-builder-33-col panel-builder-mid')
+# image = panelMid.find('div', class_ = 'image' )
+# img_tag = image.find('img')
+# img_src = img_tag.get('src','')
+# divName = panelMid.find('div', class_ = 'text col-md-8')
+# innerDivName = divName.find('p').find('a')
+# name = innerDivName.get_text(strip=True)
+# # print(name)
+# Allnames.append(name)
+# # print(img_src)
+# imageURL.append(img_src)
+# innerLink = panelMid.find('a', href=True)
+# innerURL = innerLink['href']
+# # print(innerURL)
+# Links.append(innerURL)
+# innerResponse = requests.get(innerURL)
+# innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
+# contact = innerSoup.find('div', 'contact-info primary')
+# contact2 = contact.find('a')
+# email = contact2.get_text(strip=True)
+# # print(email)
+# Allmails.append(email)
 
 
 #  RIGHTPANEL
-panelRight = main_parsys.find('div', class_='col-sm-4 panel-builder-33-col panel-builder-right')
-divName = panelRight.find('div', class_ = 'text col-md-8')
-innerDivName = divName.find('p').find('a')
-name = innerDivName.get_text(strip=True)
-# print(name)
-Allnames.append(name)
-innerLink = panelRight.find('a', href=True)
-innerURL = innerLink['href']
-# print(innerURL)
-Links.append(innerURL)
-innerResponse = requests.get(innerURL)
-innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
+# panelRight = main_parsys.find('div', class_='col-sm-4 panel-builder-33-col panel-builder-right')
+# divName = panelRight.find('div', class_ = 'text col-md-8')
+# innerDivName = divName.find('p').find('a')
+# name = innerDivName.get_text(strip=True)
+# # print(name)
+# Allnames.append(name)
+# innerLink = panelRight.find('a', href=True)
+# innerURL = innerLink['href']
+# # print(innerURL)
+# Links.append(innerURL)
+# innerResponse = requests.get(innerURL)
+# innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
+#
+# contact = innerSoup.find('div', 'contact-info primary')
+# if contact:
+#
+#     contact2 = contact.find('a')
+#     email = contact2.get_text(strip=True)
+#     # print(email)
+#     Allmails.append(email)
+# else:
+#     Allmails.append("Not Found")
 
-contact = innerSoup.find('div', 'contact-info primary')
-if contact:
-
-    contact2 = contact.find('a')
-    email = contact2.get_text(strip=True)
-    # print(email)
-    Allmails.append(email)
-else:
-    Allmails.append("Not Found")
-
-print("All mails : ", Allmails)
-print("Name : ", Allnames)
-print("URL to profile : ", Links)
+# print("All mails : ", Allmails)
+# print("Name : ", Allnames)
+# print("URL to profile : ", Links)
