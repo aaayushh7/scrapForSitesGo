5a2397324c5f3f56cc65baf97ef2ad2df06022af
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-22 03:06:35 +0530

    def, avoided repetiotion, formatng

--- scrap.py
+++ scrap.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from gspread_formatting import *
+
 
 scope = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -23,115 +25,55 @@ soup = BeautifulSoup(html.text, "html.parser")
 
 main_parsys = soup.find('div', class_ = "main parsys")
 # print(main_parsys)
-imageURL=[]
+# imageURL=[]
 Allnames =[]
 Allmails=[]
 Links=[]
 #   LEFT PANEL
-panelLeft = main_parsys.findAll('div', class_ = 'col-sm-4 panel-builder-33-col panel-builder-left')
 
-for panel in panelLeft:
-    divName = panel.find('div', class_='text col-md-8')
-    if divName:
-        # Process divName as needed
-        innerDivName = divName.find('p').find('a')
-        if innerDivName:
-            name = innerDivName.get_text(strip=True)
+def scrape_panel(panel):
+    for panel_item in panel:
+        div_name = panel_item.find('div', class_='text col-md-8')
+        if div_name:
+            inner_div_name = div_name.find('p').find('a')
+            name = inner_div_name.get_text(strip=True) if inner_div_name else "Name not found"
             Allnames.append(name)
-        else:
-            Allnames.append("Name not found")
-    else:
-        print("No matching div found in this panel.")
-    innerLink = panel.find('a', href=True)
-    if innerLink:
-        innerURL = innerLink['href']
-        Links.append(innerURL)
-        innerResponse = requests.get(innerURL)
-        innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
-        contact = innerSoup.find('div', 'contact-info primary')
-        if contact:
-            contact2 = contact.find('a')
-            if contact2:
-                email = contact2.get_text(strip=True)
-                # print(email)
-                Allmails.append(email)
+
+            inner_link = panel_item.find('a', href=True)
+            if inner_link:
+                inner_url = inner_link['href']
+                Links.append(inner_url)
+
+                try:
+                    inner_response = requests.get(inner_url)
+                    inner_soup = BeautifulSoup(inner_response.text, 'html.parser')
+                    contact = inner_soup.find('div', 'contact-info primary')
+
+                    if contact:
+                        contact_email_tag = contact.find('a')
+                        if contact_email_tag:
+                            contact_email = contact_email_tag.get_text(strip=True)
+                            Allmails.append(contact_email)
+                        else:
+                            Allmails.append("No Email in Contact Info")
+                    else:
+                        Allmails.append("No Contact Info")
+                except Exception as e:
+                    print(f"Error scraping inner page: {e}")
             else:
-                Allmails.append("No email found in Contact")
+                Links.append("N/A")
+                Allmails.append("N/A")
         else:
-            Allmails.append("No Contact Info")
-    else:
-        Links.append("No profile URL found")
+            print("No matching div found in this panel.")
 
+panel_left = main_parsys.findAll('div', class_='col-sm-4 panel-builder-33-col panel-builder-left')
+panel_mid = main_parsys.findAll('div', class_='col-sm-4 panel-builder-33-col panel-builder-mid')
+panel_right = main_parsys.findAll('div', class_='col-sm-4 panel-builder-33-col panel-builder-right')
 
-panelMid = main_parsys.findAll('div', class_='col-sm-4 panel-builder-33-col panel-builder-mid')
-
-for midPanel in panelMid:
-    divName = midPanel.find('div', class_="text col-md-8")
-    if divName:
-        innerDivName = divName.find('p').find('a')
-        if innerDivName:
-            name = innerDivName.get_text(strip=True)
-            Allnames.append(name)
-        else:
-            Allnames.append("Name not found")
-
-        innerLink = divName.find('a', href=True)
-        if innerLink:
-            innerURL = innerLink['href']
-            Links.append(innerURL)
-            innerResponse = requests.get(innerURL)
-            innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
-            contact = innerSoup.find('div', 'contact-info primary')
-            if contact:
-                contact2 = contact.find('a')
-                if contact2:
-                    email = contact2.get_text(strip=True)
-                    # print(email)
-                    Allmails.append(email)
-                else:
-                    Allmails.append("No email found in Contact")
-            else:
-                Allmails.append("No Contact Info")
-        else:
-            Links.append("No profile URL found")
-    else:
-        print("No matching div found in this panel.")
-
-
-
-panelRight = main_parsys.findAll('div', class_='col-sm-4 panel-builder-33-col panel-builder-right')
-
-for rightPanel in panelRight:
-    divName = rightPanel.find('div', class_="text col-md-8")
-    if divName:
-        innerDivName = divName.find('p').find('a')
-        if innerDivName:
-            name = innerDivName.get_text(strip=True)
-            Allnames.append(name)
-        else:
-            Allnames.append("Name not found")
-
-        innerLink = divName.find('a', href=True)
-        if innerLink:
-            innerURL = innerLink['href']
-            Links.append(innerURL)
-            innerResponse = requests.get(innerURL)
-            innerSoup = BeautifulSoup(innerResponse.text, 'html.parser')
-            contact = innerSoup.find('div', 'contact-info primary')
-            if contact:
-                contact2 = contact.find('a')
-                if contact2:
-                    email = contact2.get_text(strip=True)
-                # print(email)
-                    Allmails.append(email)
-                else:
-                    Allmails.append("No email found in Contact")
-            else:
-                Allmails.append("No Contact Info")
-        else:
-            Links.append("No profile URL found")
-    else:
-        print("No matching div found in this panel.")
+# Scrape each panel
+scrape_panel(panel_left)
+scrape_panel(panel_mid)
+scrape_panel(panel_right)
 
 
 
@@ -145,6 +87,24 @@ headers = ["Faculty Names", "Email", "Current Website"]
 sheet.append_row(headers)
 data = list(zip(Allnames, Allmails, Links))
 sheet.append_rows(data, value_input_option='USER_ENTERED')
+
+# cell_range = sheet.range(f'A1:C{len(Allnames) + 1}')  # Including the header row
+#
+# # Apply formatting to cells
+# for cell in cell_range:
+#     if cell.row == 1:  # Header row
+#         cell.color = (0.8, 0.8, 0.8)  # Gray background
+#         cell.bold = True
+#     elif cell.col == 2 or cell.col == 3:  # Email and URL columns
+#         if not cell.value:
+#             cell.color = (1, 0.8, 0.8)  # Red background
+#
+# # Update the formatted cells back to the sheet
+# sheet.update_cells(cell_range)
+# Get the entire range of cells
+
+
+
 
 
 
